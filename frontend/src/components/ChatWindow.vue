3bd2b4ebacc527db084986249e21e18bdfba1f1d
Helena Converse <helena.converse@wwt.com>
2024-12-17 17:04:39 -0600

    fixes bot message formatting

--- frontend/src/components/ChatWindow.vue
+++ frontend/src/components/ChatWindow.vue
@@ -5,13 +5,13 @@
     </div>
     <div class="flex-1 overflow-y-auto p-4 mt-16">
       <div v-for="(message, index) in messages" :key="index" class="mb-2">
-        <div :class="{'text-right': message.isUser}">
+        <div :class="{'text-right': message.isUser, 'text-left': !message.isUser}">
           <span :class="{'bg-blue-500 text-white': message.isUser, 'bg-gray-300': !message.isUser}" class="inline-block p-2 rounded">
             <i v-if="message.isUser" class="fas fa-user"></i>
             <i v-else class="fas fa-robot"></i>
-            {{ message.text }}
+            <span v-html="formatMessage(message.text)"></span>
           </span>
-          <button v-if="!message.isUser && showAddToKnowledgeButton" @click="addToKnowledge(message, messages[index - 1])" class="ml-2 p-1 bg-green-500 text-white rounded" title="Add to Knowledge">
+          <button v-if="!message.isUser && showAddToKnowledgeButton" @click="addToKnowledge(message, messages[index - 1])" class="ml-2 p-1 bg-green-500 text-white rounded float-right" title="Add to Knowledge">
             <i class="fas fa-plus"></i>
           </button>
         </div>
@@ -83,6 +83,9 @@ export default {
     },
     addToKnowledge(botMessage, userMessage) {
       this.$emit('add-to-knowledge', { botMessage, userMessage });
+    },
+    formatMessage(message) {
+      return message.replace(/\n/g, '<br>')
     }
   }
 }
